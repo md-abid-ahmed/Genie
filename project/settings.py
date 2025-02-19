45f1081c6561be9b3bd2828c5eaeaf7ec6d25ee5
md-abid-ahmed <94798371+md-abid-ahmed@users.noreply.github.com>
2025-02-19 23:41:30 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -13,17 +13,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
